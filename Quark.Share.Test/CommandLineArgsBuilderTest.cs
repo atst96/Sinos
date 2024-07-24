eee01e503aa44093de7b587cb808018b72aae5cc
atst_96 <tnkjpei04@gmail.com>
2024-07-25 00:14:19 +0900

    add: コマンド組み立てクラスを追加

---
+++ Quark.Share.Test/CommandLineArgsBuilderTest.cs
@@ -0,0 +1,71 @@
+﻿using Quark.Utils;
+
+namespace Quark.Share.Test;
+
+public class CommandLineArgsBuilderTest
+{
+    [Fact]
+    public void TestEmpty()
+    {
+        var expected = "";
+        var actual = CommandLineArgsBuilder.Create()
+            .ToString();
+
+        Assert.Equal(expected, actual);
+    }
+
+    // FIXME: テストを細かく
+    [Theory]
+    [InlineData("name", null, "-name")]        // 値未指定
+    [InlineData("name", "", "-name \"\"")]     // 値が空文字列
+    [InlineData("name", "test", "-name test")] // 値がエスケープ不要
+    [InlineData("name", " ", "-name \" \"")]   // 値がスペース単体
+    [InlineData("name", "  ", "-name \"  \"")] // 値がスペース複数
+    [InlineData("name", "\"", "-name \"\\\"\"")] // 値がダブルクォーテーション単体
+    [InlineData("name", "\\", "-name \"\\\\\"")] // 値がバックスラッシュ単体
+    public void TestSingleOption(string argName, string? argValue, string expected)
+    {
+        var actual = CommandLineArgsBuilder.Create()
+            .AppendShotArg(argName, argValue)
+            .ToString();
+
+        Assert.Equal(expected, actual);
+    }
+
+    // FIXME: テストを細かく
+    [Theory]
+    [InlineData("cmd", "cmd")]         // エスケープなし
+    [InlineData("", "\"\"")]           // 空文字列
+    [InlineData(" ", "\" \"")]         // スペースのみ
+    [InlineData("  ", "\"  \"")]       // スペースのみ
+    [InlineData("\"", "\"\\\"\"")]     // escape: only double quote
+    [InlineData("\\", "\"\\\\\"")]     // escape: back slash
+    [InlineData(" cmd", "\" cmd\"")]   // 始端にエスケープ検出
+    [InlineData("cmd ", "\"cmd \"")]   // 終端にエスケープ検出
+    [InlineData("c m d", "\"c m d\"")] // 文字列の途中にエスケープ検出
+    public void TestArg(string arg, string expected)
+    {
+        var actual = CommandLineArgsBuilder.Create()
+            .AppendArg(arg)
+            .ToString();
+
+        Assert.Equal(expected, actual);
+    }
+
+    // FIXME: テストを細かく
+    [Fact]
+    public void TestOptArg1()
+    {
+        var actual = CommandLineArgsBuilder.Create()
+            .AppendShotArg("arg1")
+            .AppendArg("value a")
+            .AppendArg("value-b")
+            .AppendShotArg("arg2", "value")
+            .AppendShotArg("arg3", "value c")
+            .ToString();
+
+        var expected = "-arg1 \"value a\" value-b -arg2 value -arg3 \"value c\"";
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Quark.Share/Utils/CommandLineArgsBuilder.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Quark.Utils;
+
+public class CommandLineArgsBuilder
+{
+    private StringBuilder _stringBuilder;
+
+    public CommandLineArgsBuilder()
+    {
+        this._stringBuilder = new();
+    }
+
+    public static CommandLineArgsBuilder Create() => new();
+
+    private bool _isAppended = false;
+
+    private void AppendDelimiter()
+    {
+        var sb = this._stringBuilder;
+        if (sb.Length > 0)
+        {
+            sb.Append(' ');
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public CommandLineArgsBuilder AppendShotArg(string argName, string? argValue = null)
+    {
+        var sb = this._stringBuilder;
+
+        this.AppendDelimiter();
+        sb.Append('-');
+        sb.Append(argName);
+
+        if (argValue != null)
+            _ = this.AppendArg(argValue);
+
+        return this;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public CommandLineArgsBuilder AppendArg(string arg)
+    {
+        var sb = this._stringBuilder;
+
+        scoped var chars = arg.AsSpan();
+
+        this.AppendDelimiter();
+
+        if (string.IsNullOrEmpty(arg))
+        {
+            sb.Append("\"\"");
+            return this;
+        }
+
+        // エスケープする文字がなければ
+        int pre = 0;
+        int cur = FindNextEscape(chars);
+        if (cur < 0)
+        {
+            // エスケープ対象の文字が1つもなければエスケープせずに登録
+            sb.Append(arg);
+            return this;
+        }
+
+        sb.Append('"');
+
+        while (cur >= 0)
+        {
+            if (pre < cur)
+            {
+                sb.Append(chars[pre..cur]);
+            }
+
+            char c = chars[cur];
+            if (c == ' ')
+                sb.Append(c);
+            else if (c == '"')
+                sb.Append("\\\"");
+            else if (c == '\\')
+                sb.Append("\\\\");
+
+            // 次にエスケープが必要な文字位置を探す
+            pre = cur + 1;
+            cur = FindNextEscape(chars, pre);
+        }
+
+        if (pre < arg.Length)
+        {
+            // 最後の検出位置から末尾までを追記
+            sb.Append(chars[pre..arg.Length]);
+        }
+
+        sb.Append('"');
+
+        return this;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int FindNextEscape(scoped ReadOnlySpan<char> text, int findIdx = 0)
+    {
+        for (int idx = findIdx; idx < text.Length; ++idx)
+        {
+            char c = text[idx];
+            if (c is ' ' or '\"' or '\\')
+            {
+                return idx;
+            }
+        }
+
+        return -1;
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+        => this._stringBuilder.ToString();
+}
